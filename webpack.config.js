97ca391390af03b7bf5917ad862037cf73bbbffe
zhaocxu1996 <zhaocxu1996@github.com>
2019-10-01 18:37:21 -0400

    initialize navi bar

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,16 @@ module.exports = {
             }, {
                 loader: "sass-loader" // compiles Sass to CSS
             }]
+        },  {
+            test: /\.(png|jpg|gif)$/,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: 'images/background.jpg',
+                    publicPath: '/'
+                }
+            }]
         }]
     }
+    
 };
